ddcd55f8766741e87e7d2e8b9e91d3e5bc678131
Itai Barkav <itaibarkav@gmail.com>
2021-01-24 00:04:18 +0200

    Update package.json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require("express");
 const path = require("path");
 
-const app = exspress();
+const app = express();
 
-app.use(express.static(__dirname + "/dist/allcg"));
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/allcg"));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/allcg/index.html"));
-});
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/allcg/" })
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
