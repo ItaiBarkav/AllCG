e95cad6b57f332acd261cc3155f49f6261cb41bc
Itai Barkav <itaibarkav@gmail.com>
2021-01-23 16:11:47 +0200

    Add search component with basic css

--- allcg/src/app/app.module.ts
+++ allcg/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ allcg/src/app/search/search.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SearchComponent {
+  constructor() {}
+}
